da51994d17ee4692dfe39a8b9b8c2f55f7f5a60f
bioinfo-ibms-pumc <54611317+bioinfo-ibms-pumc@users.noreply.github.com>
2024-05-21 09:39:27 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -32,16 +32,16 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.myTable.setRowCount(1)
 
     def check_float(self,s):
-        if s.count('.') == 1:  # 判断小数点个数
-            sl = s.split('.')  # 按照小数点进行分割
-            left = sl[0]  # 小数点前面的
-            right = sl[1]  # 小数点后面的
+        if s.count('.') == 1:  # judge decimal point
+            sl = s.split('.')  
+            left = sl[0]  
+            right = sl[1]  
             if left.startswith('-') and left.count('-') == 1 and right.isdigit():
-                lleft = left.split('-')[1]  # 按照-分割，然后取负号后面的数字
+                lleft = left.split('-')[1]  # negative
                 if lleft.isdigit():
                     return False
             elif left.isdigit() and right.isdigit():
-                # 判断是否为正小数
+                
                 return True
         elif s.isdigit():
             s = int(s)
