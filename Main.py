50e83288d0fc374f65fb941abd70fa22dd364206
bioinfo-ibms-pumc <54611317+bioinfo-ibms-pumc@users.noreply.github.com>
2024-06-20 14:09:12 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,131 @@
+#########################################################################
+# File Name: Main.py
+# > Author: CaoYinghao
+# > Mail: caoyinghao@gmail.com
+# Created Time: Thu May 16 10:05:37 2024
+#########################################################################
+#! /usr/bin/python
+
+import sys
+import os
+
+from PyQt5.QtWidgets import QMainWindow,QApplication,QDesktopWidget
+from window import Ui_MainWindow
+from PyQt5.QtWidgets import QTableWidgetItem,QMessageBox
+from PyQt5.QtCore import Qt
+from core import SearchModel
+import sqlite3
+
+class MainWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+        if not os.path.exists("smart.db"):
+            print("Warning, no smart database found.")
+        self.conn = sqlite3.connect("smart.db")
+        self.cur = self.conn.cursor()
+        self.setupUi(self)
+        self.predBtn.clicked.connect(self.pred)
+        self.sm = SearchModel()
+
+        titles = ['Input', "Rank", 'Formula', 'm/z','ppm', 'score', 'DB']
+        for i in range(len(titles)):
+            item = QTableWidgetItem()
+            item.setText(titles[i])
+            self.myTable.setHorizontalHeaderItem(i, item)
+        self.myTable.setRowCount(1)
+
+    def check_float(self,s):
+        if s.count('.') == 1:  # judge decimal point
+            sl = s.split('.')  
+            left = sl[0]  
+            right = sl[1]  
+            if left.startswith('-') and left.count('-') == 1 and right.isdigit():
+                lleft = left.split('-')[1]  # negative
+                if lleft.isdigit():
+                    return False
+            elif left.isdigit() and right.isdigit():
+                
+                return True
+        elif s.isdigit():
+            s = int(s)
+            if s != 0:
+                return True
+    def pred(self):
+        text = self.input.toPlainText()
+        print("text",text)
+        if text.strip() == "":
+            QMessageBox.warning(None, "Error", "No m/z found. Please enter m/z value line by line! ")
+            return
+        ppm = self.ppm.text()
+        if not self.check_float(ppm):
+            QMessageBox.warning(None, "Error", "PPM need integer/float value.")
+            return
+
+        allmz = []
+        allres = []
+        iontag = self.group.checkedId()
+        rowC = 0
+
+        for mz in text.strip().split("\n"):
+            if mz == "":continue
+            if not self.check_float(mz):
+                QMessageBox.warning(None, "Error", mz + " need integer/float value.")
+                return
+            allmz.append(mz)
+            res = self.sm.search(mz,iontag,self.cur,ppm_error=ppm,verbose=False)
+            rowC += len(res)
+            allres.append(res)
+        self.myTable.clear()
+        self.myTable.setRowCount(rowC)
+        titles = ['Input', "Rank", 'Formula', 'm/z','ppm', 'score', 'DB']
+        for i in range(len(titles)):
+            item = QTableWidgetItem()
+            item.setText(titles[i])
+            self.myTable.setHorizontalHeaderItem(i, item)
+        c = 0
+        for i in range(len(allres)):
+            res = allres[i]
+
+            for j in range(len(res)):
+                if j > 3:break
+                # print(res[j],c)
+                item = QTableWidgetItem()
+                item.setText(str(allmz[i]))
+                item.setTextAlignment(Qt.AlignCenter)
+                self.myTable.setItem(c, 0, item)
+                if len(res[j]) == 0:
+                    for k in range(len(titles)-1):
+                        item = QTableWidgetItem()
+                        item.setText("-")
+                        item.setTextAlignment(Qt.AlignCenter)
+                        self.myTable.setItem(c, k + 1, item)
+                else:
+                    for k in range(len(res[j])):
+                        txt = str(res[j][k])
+                        if k == 0:
+                            txt = str(j)
+                        item = QTableWidgetItem()
+                        item.setText(txt)
+                        item.setTextAlignment(Qt.AlignCenter)
+                        self.myTable.setItem(c,k+1,item)
+                c += 1
+
+        pass
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        newLeft = int((screen.width() - size.width()) / 2)
+        newTop = int((screen.height() - size.height()) / 2)
+        self.move(newLeft, newTop)
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.center()
+    win.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
+
---
+++ SMART.py
@@ -0,0 +1,129 @@
+#########################################################################
+# File Name: SMART.py
+# > Author: CaoYinghao
+# > Mail: caoyinghao@gmail.com
+# Created Time: Thu May 16 10:05:37 2024
+#########################################################################
+#! /usr/bin/python
+
+import glob
+import sys
+import re
+import os
+import argparse
+from core import SearchModel
+import sqlite3
+
+
+
+def get_parser():
+    desc = """Program: SMART_CMD
+Version: 1.0
+ Email : <yhcao@ibms.pumc.edu.cn>
+      """
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=desc)
+
+    parser.add_argument('-i', '--input', required=True,
+                           help="Input file/mz_value for formula assignment (Only table format supported).")
+    # runparser.add_argument('-o', '--output', required = True, help="Output prefix for metabolite extraction.")
+    parser.add_argument('-p', '--polarity', default="+",
+                        help="Polarity information for formula assignment. (Default: +, [-,0])")
+    parser.add_argument('-d', '--database', required=True,
+                        help="SMART-Database file for formula assignment.")
+    #parser.add_argument('-e', '--extension', action="store_true",
+    #                       help="Include extension candidates for formula assignment. ")
+    parser.add_argument('-m', '--ppm', default=5, help="PPM threshold for formula assignment (Default: 5).")
+    parser.add_argument('-v', '--verbose', default=False,
+                        help="Peak group per mz for formula assignment (Default: 5; Only integer large than 1).")
+
+    return parser
+
+def check_float(s):
+    if s.count('.') == 1:  # 判断小数点个数
+        sl = s.split('.')  # 按照小数点进行分割
+        left = sl[0]  # 小数点前面的
+        right = sl[1]  # 小数点后面的
+        if left.startswith('-') and left.count('-') == 1 and right.isdigit():
+            lleft = left.split('-')[1]  # 按照-分割，然后取负号后面的数字
+            if lleft.isdigit():
+                return False
+        elif left.isdigit() and right.isdigit():
+            # 判断是否为正小数
+            return True
+    elif s.isdigit():
+        s = int(s)
+        if s != 0:
+            return True
+
+def run_cmd(args):
+    sm = SearchModel()
+    if os.path.exists(args.input):
+        iontag = -1
+        if args.polarity == "-":
+            iontag = 1
+        elif args.polarity == "+":
+            iontag = 0
+        out = open(args.input.split("/")[-1] +".smart_results.txt","w")
+        out.write("mz\tRawFormula\tID\tFormula\tm/z\tppm\tscore\tDB\n")
+        conn = sqlite3.connect(args.database)
+        cur = conn.cursor()
+        i = 0
+        with open(args.input,"r") as infile:
+            for line in infile:
+                mzs = line.strip().split("\t")
+                if mzs[0] == "mz":continue
+                mz = mzs[0]
+                i += 1
+                #res = sm.search(mz,iontag,cur,args.extension,ppm_error=args.ppm,verbose=False)
+                res = sm.search(mz,iontag,cur,ppm_error=args.ppm,verbose=False)
+                if len(res) > 0:
+                    #for s in res:
+                    #print(i,mz,s)
+                    out.write(line.strip() + "\t" + "\t".join(list(map(str,res[0]))) +"\n")
+                else:
+                    out.write(line.strip() + "\n")
+        out.close()
+        print("Results is saved to " + args.input + ".smart_results.txt")
+                
+    else:
+        mz = args.input
+        if not check_float(str(args.ppm)):
+            print("Error, PPM needs integer/float value.")
+            exit()
+        if not check_float(mz):
+            print("Error, "+ mz+ " needs integer/float value.")
+            exit()
+        if not os.path.exists(args.database):
+            print("Error, "+ database+ " is not exists")
+            exit()
+        conn = sqlite3.connect(args.database)
+        cur = conn.cursor()
+        mz = args.input
+        iontag = -1
+        if args.polarity == "-":
+            iontag = 1
+        elif args.polarity == "+":
+            iontag = 0
+        #res = sm.search(mz,iontag,cur,args.extension,ppm_error=args.ppm,verbose=False)
+        res = sm.search(mz,iontag,cur,ppm_error=args.ppm,verbose=False)
+        title = ["MZ","id","formula","mz","ppm","score","source"]
+        print("{0:3s}{1:^15s}{2:^10s}{3:^15s}{4:^15s}{5:^10s}{6:^10s}{7:^10s}".format("Rank",title[0],title[1],title[2],title[3],title[4],title[5],title[6]))
+        rank = 0
+        for s in res:
+            rank += 1
+            if rank > 10:break
+            if len(s) > 0:
+                print("{0:3d}{1:^15s}{2:^10d}{3:^15s}{4:^15.4f}{5:^10.3f}{6:^10.3f}{7:^10s}".format(rank,mz,s[0],s[1],s[2],s[3],s[4],s[5]))
+            else:
+                print("{0:3d}{1:^15s}".format(rank,mz))
+
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.input is not None:
+        run_cmd(args)
+    else:
+        parser.print_help()
+    exit()
